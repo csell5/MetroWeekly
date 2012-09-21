24cd7c5ea9047f8292dc372be0fce539961c345e
Clark Sell <clark.sell@csell.net>
2012-09-21 15:15:54 -0500

    inital checkin

---
+++ source/app/js/userSettings.js
@@ -0,0 +1,11 @@
+﻿/*
+(function () {
+    "use strict";
+    var page = WinJS.UI.Pages.define("/html/userSettings.html", {
+        ready: function (element, options) {
+            WinJS.log && WinJS.log("To show the settings charm, invoke the charm bar by swiping your finger on the right edge of the screen or bringing your mouse to the lower-right corner of the screen, then select Settings. Or you can just press Windows logo + i. To dismiss the settings charm, tap in the application, swipe a screen edge, right click, invoke another charm or application.", "sample", "status");
+        }
+    });
+
+})();
+*/
