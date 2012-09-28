3d45602ce5d2206d7a64428a745690939901c7b2
Clark Sell <clark.sell@csell.net>
2012-09-28 15:32:47 -0500

    added blue border to the top

--- source/app/js/data.js
+++ source/app/js/data.js
@@ -10,6 +10,16 @@
         articles.forEach(function (i) {
             articleList.push(i); 
         })
+
+
+        var groupCount = {};
+        var maxItems = 6;
+
+        var filteredList = articleList.createFiltered(function _filterItems(item) {
+            groupCount[item.group] = groupCount[item.group] ? groupCount[item.group] + 1 : 1;
+            return groupCount[item.group] <= maxItems;
+        });
+
     });
 
     // Sorts the groups
